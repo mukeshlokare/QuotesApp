1c31a147c5eac82c097f09edb29309c8b56e8b5e
pdichone <pdichone@gmail.com>
2019-10-02 12:13:45 -0700

    Initial Commit

---
+++ QuakesApp-New-XC11/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuakesApp-New-XC11
+//
+//  Created by Paulo Dichone on 10/2/19.
+//  Copyright © 2019 Paulo Dichone. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
